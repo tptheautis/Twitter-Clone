1e11d1472a298fdf918eebfd03559d23ef86b749
Theautis <116971196+tptheautis@users.noreply.github.com>
2023-04-15 14:40:55 -0700

    added main container and columns in home page function

--- src/home.js
+++ src/home.js
@@ -1,10 +1,17 @@
 function createHomePage() {
   const content = document.getElementById('content');
-  const div = document.createElement('div');
-  div.classList.add('test');
-  const text = document.createTextNode('yes');
-  div.append(text);
-  content.append(div);
+  const mainContainer = document.createElement('div');
+  const column1 = document.createElement('div');
+  const column2 = document.createElement('div');
+  const column3 = document.createElement('div');
+  mainContainer.classList.add('mainContainer');
+  column1.classList.add('column1');
+  column2.classList.add('column2');
+  column3.classList.add('column3');
+  content.append(mainContainer);
+  mainContainer.append(column1);
+  mainContainer.append(column2);
+  mainContainer.append(column3);
 }
 
 export default createHomePage();
