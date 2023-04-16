e379e6957a470aa24adcace03b21639b78c9c4aa
Theautis <116971196+tptheautis@users.noreply.github.com>
2023-04-15 17:45:37 -0700

    defined left sidebar elements in dom

--- src/home.js
+++ src/home.js
@@ -1,5 +1,6 @@
 function createHomePage() {
   const content = document.getElementById('content');
+  // Base Layout
   const mainContainer = document.createElement('div');
   const column1 = document.createElement('div');
   const column2 = document.createElement('div');
@@ -12,6 +13,17 @@ function createHomePage() {
   mainContainer.append(column1);
   mainContainer.append(column2);
   mainContainer.append(column3);
+
+  // Left Sidebar
+  const home = document.createElement('div');
+  const explore = document.createElement('div');
+  const notifications = document.createElement('div');
+  const messages = document.createElement('div');
+  const bookmarks = document.createElement('div');
+  const twitterBlue = document.createElement('div');
+  const profile = document.createElement('div');
+  const more = document.createElement('div');
+  const tweet = document.createElement('tweet');
 }
 
 export default createHomePage();
